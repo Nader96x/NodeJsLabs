f66cfad7b8ceb9ea8a563addd3a86f50f0a12d85
nader mohammed <nader.mohammed.20x@gmail.com>
2023-02-23 13:24:53 +0200

    update class updated

--- Controllers/classController.js
+++ Controllers/classController.js
@@ -52,24 +52,42 @@ exports.addClass=(request,response,next)=>{
             .catch(error=>next(error))
 }
 
-exports.updateClass=(request,response,next)=>{
-    ClassSchema.updateOne({
-        _id:request.body.id
-    },{
-        $set:{
-            name:request.body.name,
-            supervisor:request.body.supervisor,
-            children:request.body.children
+exports.updateClass=async (request,response,next)=>{
+    try{
+        let data ='';
+        if(request.body.supervisor){
+            data = await TeacherSchema.findOne({_id:request.body.supervisor},{_id:1})
+            if(data==null) throw Error("Teacher not Found");
         }
-    }).then(data=>{
+
+        if(request.body.children){
+            data = await ChildSchema.find({_id:{$in:request.body.children}},{_id:1})
+            data = data.map(obj=>Number(obj.id));
+            request.body.children = request.body.children.map(id=>Number(id));
+            request.body.children = removeDuplicates(request.body.children);
+            if(data.length!=request.body.children.length){
+                let rest = request.body.children.filter(n => !data.includes(n))
+                throw Error(rest + " Childern not Found");
+            }
+        }
+
+        data = await ClassSchema.updateOne({
+            _id:request.body.id
+        },{
+            $set:{
+                name:request.body.name,
+                supervisor:request.body.supervisor,
+                children:request.body.children
+            }
+        })
         if(data.matchedCount==0)
-        {
-            next(new Error("Child not found"));
-        }
+            throw new Error("Class not found")
         else
-        response.status(200).json({data});
-    })
-    .catch(error=>next(error));
+            response.status(200).json({data:await ClassSchema.findOne({_id:request.body.id})});
+    }
+    catch(error){
+        next(error);
+    }
 }
 exports.deleteClass=(request,res,next)=>{
     ClassSchema.deleteOne({_id:request.body.id})
